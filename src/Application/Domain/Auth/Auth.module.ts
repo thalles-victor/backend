31c8c09ab46b71bc4e61b3ba17c8703237fe56f0
thalles-victor <thalles.victor2012gmail.com>
2024-07-10 08:42:46 -0300

    :heavy_plus_sign: dep: add jwt module to create security tokens. more information about JWT available in: https://jwt.io/ more information about JWT npm library: https://docs.nestjs.com/security/authentication#jwt-token

--- src/Application/Domain/Auth/Auth.module.ts
+++ src/Application/Domain/Auth/Auth.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { StudentModule } from '../Student/Student.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './Auth.service';
+import { AuthController } from './Auth.controller';
 
-@Module({})
+@Module({
+  imports: [
+    StudentModule,
+    JwtModule.register({
+      global: false,
+      secret: 'SECRET',
+      signOptions: { expiresIn: '10min' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
 export class AuthModule {}
