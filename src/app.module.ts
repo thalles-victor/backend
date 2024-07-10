20669631c63f6b5e8929bbd25f42de5a28dd4c2d
thalles-victor <thalles.victor2012gmail.com>
2024-07-10 09:06:19 -0300

    :heavy_plus_sign: dep: add Joi to validate env properties.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { UserModule } from './Application/Domain/User/user.module';
 import { CourseModule } from './Application/Domain/Course/Course.module';
 import { StudentModule } from './Application/Domain/Student/Student.module';
 import { AuthModule } from './Application/Domain/Auth/Auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UserModule, CourseModule, StudentModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        ACCESS_TOKEN_SECRET: Joi.string(),
+        ACCESS_TOKEN_EXPIRATION: Joi.string(),
+        REFRESH_TOKEN_SECRET: Joi.string(),
+        REFRESH_TOKEN_EXPIRATION: Joi.string(),
+      }),
+    }),
+    UserModule,
+    CourseModule,
+    StudentModule,
+    AuthModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
