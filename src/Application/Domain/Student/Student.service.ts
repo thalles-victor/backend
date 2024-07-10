7c1f51235b9a458a1b4b46d446a3c45a22fb242e
thalles-victor <thalles.victor2012gmail.com>
2024-07-10 15:35:03 -0300

    :sparkles: feat: implement methods.

--- src/Application/Domain/Student/Student.service.ts
+++ src/Application/Domain/Student/Student.service.ts
@@ -1,27 +1,43 @@
-import { Injectable } from '@nestjs/common';
-
-export type User = {
-  userId: number;
-  username: string;
-  password: string;
-};
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  StudentEntity,
+  UpdateStudentEntity,
+} from 'src/Application/Entities/Student.entity';
+import { StudentsTypeOrmRepository } from 'src/Application/Repositories/Students/StudentsTypeOrm.repository';
 
 @Injectable()
 export class StudentService {
-  private readonly users: User[] = [
-    {
-      userId: 0,
-      username: 'jhon',
-      password: 'minhasenha123',
-    },
-    {
-      userId: 1,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(private readonly studentsRepository: StudentsTypeOrmRepository) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((_user_) => _user_.username === username);
+  async getByEmail(email: string): Promise<StudentEntity | null> {
+    const student = await this.studentsRepository.getOnByEmail(email);
+
+    return student ?? null;
+  }
+
+  async getById(email: string): Promise<StudentEntity | null> {
+    const student = await this.studentsRepository.getOneById(email);
+
+    return student ?? null;
+  }
+
+  async deleteById(id: string): Promise<'success' | 'fail'> {
+    const result = await this.studentsRepository.deleteById(id);
+
+    return result;
+  }
+
+  async updateStudent(id, updateEntity: UpdateStudentEntity) {
+    const userToUpdate = await this.studentsRepository.getOneById(id);
+
+    if (!userToUpdate) throw new NotFoundException('student not found');
+
+    const userUpdated = await this.studentsRepository.updateById(id, {
+      name: updateEntity.name,
+      password: updateEntity.password,
+      refresh_token: updateEntity.refresh_token,
+    });
+
+    return userUpdated;
   }
 }
