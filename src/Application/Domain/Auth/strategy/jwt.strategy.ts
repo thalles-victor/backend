16953d0ae8ff178666ad5ff74db7947cdc82cfbe
thalles-victor <thalles.victor2012gmail.com>
2024-07-10 15:49:37 -0300

    :sparkle: feat: create jwt strategy.

---
+++ src/Application/Domain/Auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { TStudentPayload } from 'src/Application/@types';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('ACCESS_TOKEN_SECRET'),
+    });
+  }
+
+  async validate(payload: TStudentPayload) {
+    return payload;
+  }
+}
