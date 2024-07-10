a8d9637245412220daa090dd1ce6fbffdb731439
thalles-victor <thalles.victor2012gmail.com>
2024-07-10 15:12:42 -0300

    :label: feat: create global types.

---
+++ src/Application/@types/index.ts
@@ -0,0 +1,13 @@
+export type TPayload = {
+  sub: string;
+  email: string;
+};
+
+export type TSignIn = {
+  email: string;
+  password: string;
+};
+
+export type TStudentPayload = {
+  sub: string;
+};
