0bf90e5e556d26e58bbea8318c9db6919907cc4b
thalles-victor <thalles.victor2012gmail.com>
2024-07-11 08:32:00 -0300

    :sparkles: feat: create signin and signup routes.

--- src/Application/Domain/Auth/Auth.controller.ts
+++ src/Application/Domain/Auth/Auth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './Auth.service';
+import { RegisterStudentDto } from '../Student/dtos/Student.dtos';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('')
-  hello() {
-    return 'hello auth path';
+  @UseGuards(AuthGuard('local'))
+  @Post('signin')
+  async login(@Request() request: any) {
+    return this.authService.signIn(request.user);
+  }
+
+  @Post('signup')
+  async register(@Body() student: RegisterStudentDto) {
+    return this.authService.signUp(student);
   }
 }
