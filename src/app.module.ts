d14c154b32fcadd44f2fd4b1ea68fee986df2435
thalles-victor <thalles.victor2012gmail.com>
2024-07-11 08:30:16 -0300

    :lock: sec: add validation on environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,23 @@ import { CourseModule } from './Application/Domain/Course/Course.module';
 import { StudentModule } from './Application/Domain/Student/Student.module';
 import { AuthModule } from './Application/Domain/Auth/Auth.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentEntity } from './Application/Entities/Student.entity';
 import { RepositoriesModule } from './Application/Repositories/Students.module';
 
+import * as Joi from 'joi';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        ACCESS_TOKEN_SECRET: Joi.string(),
+        ACCESS_TOKEN_EXPIRATION: Joi.string(),
+        REFRESH_TOKEN_SECRET: Joi.string(),
+        REFRESH_TOKEN_EXPIRATION: Joi.string(),
+      }),
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -20,14 +30,7 @@ import { RepositoriesModule } from './Application/Repositories/Students.module';
       entities: [StudentEntity],
       synchronize: true,
     }),
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        ACCESS_TOKEN_SECRET: Joi.string(),
-        ACCESS_TOKEN_EXPIRATION: Joi.string(),
-        REFRESH_TOKEN_SECRET: Joi.string(),
-        REFRESH_TOKEN_EXPIRATION: Joi.string(),
-      }),
-    }),
+
     CourseModule,
     StudentModule,
     RepositoriesModule,
