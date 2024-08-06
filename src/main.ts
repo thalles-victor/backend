4dac41a8cdd4ef04727b023017c0dd122f3526d7
thalles-victor <thalles.victor2012gmail.com>
2024-08-06 19:05:43 -0300

    :books: docs: configured swagger in project.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Course Platform')
+    .setDescription('this is backend from course platform')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/')
+    .addTag('Your Api Tag')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
